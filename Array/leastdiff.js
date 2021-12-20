c85bcddd792eb4c72351767b83a791f44cd9ec89
nimith <nimithbbz10@gmail.com>
2021-12-20 19:40:17 +0530

    Add files via upload

---
+++ Array/leastdiff.js
@@ -0,0 +1,15 @@
+const leastdiff = (num) => {
+let givenarr = num.sort((a,b) => a-b );
+let minval = givenarr[givenarr.length - 1]
+if(givenarr.length>1){
+  for(let i=givenarr.length-1;i>0;i--){
+    minval = (minval < givenarr[i]-givenarr[i-1] ? minval : givenarr[i]-givenarr[i-1])
+  }
+  return minval;
+}
+return givenarr.toString()
+
+}
+
+
+console.log(leastdiff([1, 19, -4, 31, 38, 25, 100]));
